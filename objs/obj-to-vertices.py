6491c8de0cbb735b0c2427d3a89e2c962240330f
Lucas <lucasmartinsdeb@gmail.com>
2024-11-24 21:49:45 -0300

    [wip] nova forma de renderizar pista

--- objs/obj-to-vertices.py
+++ objs/obj-to-vertices.py
@@ -59,12 +59,12 @@ def generate_vertex_list(vertices, texture_coords, faces):
             u, v = texture_coords[tex_index]
 
             # Adicionar ao vertex_list no formato desejado
-            vertex_list.append(f"{round(x-1.75, 3)}f, {y}f, {z}f, {u}f, {v}f,")
+            vertex_list.append(f"{round(x, 0)}f, {round(y, 0)}f, {round(z, 0)}f, {round(u, 0)}f, {round(v, 0)}f,")
     
     return vertex_list
 
 # Caminho para o arquivo .obj
-file_path = 'carro.obj'
+file_path = 'objs/innerPista.obj'
 
 # Parse do arquivo
 vertices, texture_coords, normals, faces = parse_obj(file_path)
